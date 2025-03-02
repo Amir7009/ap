9e4fc29de6162712342f1adf2df322363cf70d23
Amir7009 <mohebbi7009@gmail.com>
2025-03-02 13:39:00 +0330

    first practice

---
+++ Potential/Potential.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Potential {
+    public static void main(String[] args) {
+        final float G = 9.8066f;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the mass: ");
+        float m = scanner.nextFloat();
+        System.out.println("enter the height: ");
+        float h = scanner.nextFloat();
+        scanner.close();
+        System.out.println("potential= mgh= "+(m*G*h));
+    }
+}
