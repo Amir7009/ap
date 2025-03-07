615aa69f85ca02a35710becd95d29875c2e5d16a
Amir7009 <mohebbi7009@gmail.com>
2025-03-07 14:46:44 +0330

    E6_18

---
+++ Big-Java-Practices/Ch6/E/E6_18/E6_18.java
@@ -0,0 +1,21 @@
+package Ch6.E.E6_18;
+
+import java.util.Scanner;
+
+public class E6_18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+        for (int i=1 ; i < 2*n ; i++) {
+            int z = n - Math.abs(n - i);
+            for (int j=1 ; j < 2*n ; j++) {
+                if ( j <= n-z || j > n+z-1)
+                    System.out.print("   ");
+                else
+                    System.out.print(" * ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
