d30d43c95bd2ee3f8e74cbecc17b67bc83aedbe7
Amir7009 <mohebbi7009@gmail.com>
2025-06-03 17:22:09 +0330

    scraper files

---
+++ ap/exercises/ex5/scraper/Conf.java
@@ -0,0 +1,9 @@
+package ap.exercises.ex5.scraper;
+
+public class Conf {
+
+    public static final String DOMAIN_ADDRESS = "https://www.znu.ac.ir";
+
+    public static final String SAVE_DIRECTORY = "E:\\Java\\Advanced-Programming\\ap\\exercises\\ex5\\scraper\\fetched_html";
+
+}
