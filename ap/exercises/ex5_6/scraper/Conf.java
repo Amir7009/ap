1bdfea6e1f724291abc2d3f8facf61fa86064ba3
Amir7009 <mohebbi7009@gmail.com>
2025-06-04 16:21:43 +0330

    EX6_SC1_editing_packages_2

--- ap/exercises/ex5_6/scraper/Conf.java
+++ ap/exercises/ex5_6/scraper/Conf.java
@@ -4,6 +4,6 @@ public class Conf {
 
     public static final String DOMAIN_ADDRESS = "https://www.znu.ac.ir";
 
-    public static final String SAVE_DIRECTORY = "E:\\Java\\Advanced-Programming\\ap\\exercises\\ex5\\scraper\\fetched_html";
+    public static final String SAVE_DIRECTORY = "E:\\Java\\Advanced-Programming\\ap\\exercises\\ex5_6\\scraper\\fetched_html";
 
 }
