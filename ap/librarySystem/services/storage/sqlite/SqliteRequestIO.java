8b0b37eb7f9c2407f6a03d8fa69bd6325a6939b2
Amir7009 <mohebbi7009@gmail.com>
2025-06-16 23:29:32 +0330

    Library System: sqlite complete strategy v2.0.0

---
+++ ap/librarySystem/services/storage/sqlite/SqliteRequestIO.java
@@ -0,0 +1,69 @@
+package ap.librarySystem.services.storage.sqlite;
+
+import ap.librarySystem.constants.RequestType;
+import ap.librarySystem.models.borrowSystem.Request;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class SqliteRequestIO {
+
+    public static void save(ArrayList<Request> requests, String fileName) {
+        try (   // create a database connection
+                Connection connection = DriverManager.getConnection("jdbc:sqlite:" + fileName);
+                Statement statement = connection.createStatement();
+        ) {
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            statement.executeUpdate("drop table if exists requests");
+            statement.executeUpdate("create table requests (" +
+                    "borrowerStudentID string, " +
+                    "borrowedBookISBN string, " +
+                    "requestType string, " +
+                    "librarianID string)"
+            );
+
+            for (Request request : requests) {
+                statement.executeUpdate("insert into requests values('" +
+                        request.getBorrowerStudentID() + "', '" +
+                        request.getBorrowedBookISBN() + "', '" +
+                        request.getRequestType() + "', '" +
+                        request.getLibrarianID() + "')");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error writing requests to Sqlite: " + e.getMessage());
+        }
+    }
+
+    public static ArrayList<Request> load(String fileName) {
+
+        try (    // create a database connection
+                 Connection connection = DriverManager.getConnection("jdbc:sqlite:" + fileName);
+                 Statement statement = connection.createStatement();
+        ) {
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            ResultSet rs = statement.executeQuery("select * from requests");
+            ArrayList<Request> requests = new ArrayList<>();
+            while (rs.next()) {
+                Request request = new Request(
+                        rs.getString("borrowerStudentID"),
+                        rs.getString("borrowedBookISBN"),
+                        RequestType.valueOf(rs.getString("requestType")),
+                        rs.getString("librarianID")
+
+                );
+
+                requests.add(request);
+            }
+            return requests;
+
+        } catch (SQLException e) {
+            System.err.println("Error reading requests from Sqlite: " + e.getMessage());
+            return null;
+        }
+
+    }
+
+}
---
+++ ap/librarySystem/services/storage/sqlite/SqliteStudentIO.java
@@ -0,0 +1,75 @@
+package ap.librarySystem.services.storage.sqlite;
+
+import ap.librarySystem.models.Student;
+
+import java.sql.*;
+import java.util.LinkedHashMap;
+
+public class SqliteStudentIO {
+
+    public static void save(LinkedHashMap<String, Student> students, String fileName) {
+        try (   // create a database connection
+                Connection connection = DriverManager.getConnection("jdbc:sqlite:" + fileName);
+                Statement statement = connection.createStatement();
+        ) {
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            statement.executeUpdate("drop table if exists students");
+            statement.executeUpdate("create table students (" +
+                    "firstName string, " +
+                    "lastName string, " +
+                    "studyingField string, " +
+                    "studentId string, " +
+                    "membershipDate string, " +
+                    "notifications string, " +
+                    "loanHistory string)"
+            );
+
+            for (Student student : students.values()) {
+                statement.executeUpdate("insert into students values('" +
+                        student.getFirstName() + "', '" +
+                        student.getLastName() + "', '" +
+                        student.getStudyingField() + "', '" +
+                        student.getStudentID() + "', '" +
+                        student.getMembershipDate() + "', '" +
+                        student.getNotifications() + "', '" +
+                        student.getLoanHistory() + "')");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error writing students to Sqlite: " + e.getMessage());
+        }
+    }
+
+    public static LinkedHashMap<String, Student> load(String fileName) {
+
+        try (    // create a database connection
+                 Connection connection = DriverManager.getConnection("jdbc:sqlite:" + fileName);
+                 Statement statement = connection.createStatement();
+        ) {
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            ResultSet rs = statement.executeQuery("select * from students");
+            LinkedHashMap<String, Student> students = new LinkedHashMap<>();
+            while (rs.next()) {
+                Student student = new Student(
+                        rs.getString("firstName"),
+                        rs.getString("lastName"),
+                        rs.getString("studyingField"),
+                        rs.getString("studentId"),
+                        rs.getString("membershipDate"),
+                        rs.getString("notifications"),
+                        rs.getString("loanHistory")
+                );
+                students.put(student.getStudentID(), student);
+            }
+            return students;
+
+        } catch (SQLException e) {
+            System.err.println("Error reading students from Sqlite: " + e.getMessage());
+            return null;
+        }
+
+    }
+
+}
